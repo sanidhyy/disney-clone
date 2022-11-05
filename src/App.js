15ddccaf58aaf18fd3c04626f5f18e9ec05ccaaa
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-11-05 13:51:56 +0530

    header nav created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Login from "./components/Login";
+import Header from "./components/Header";
 
 // CSS
 import "./App.css";
@@ -9,6 +11,7 @@ import "./App.css";
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Login />} />
       </Routes>
---
+++ src/components/Header.jsx
@@ -0,0 +1,163 @@
+import React from "react";
+import styled from "styled-components";
+
+import logo from "../assets/images/logo.svg";
+import HomeIcon from "../assets/images/home-icon.svg";
+import SearchIcon from "../assets/images/search-icon.svg";
+import WatchlistIcon from "../assets/images/watchlist-icon.svg";
+import OriginalsIcon from "../assets/images/original-icon.svg";
+import MoviesIcon from "../assets/images/movie-icon.svg";
+import SeriesIcon from "../assets/images/series-icon.svg";
+
+const Header = () => {
+  return (
+    <Nav>
+      <Logo>
+        <img src={logo} alt="Disney+" />
+      </Logo>
+      <NavMenu>
+        <a href="/home">
+          <img src={HomeIcon} alt="Home" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src={SearchIcon} alt="Search" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src={WatchlistIcon} alt="Watchlist" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src={OriginalsIcon} alt="Originals" />
+          <span>ORIGINALS</span>
+        </a>
+        <a>
+          <img src={MoviesIcon} alt="Movies" />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src={SeriesIcon} alt="Series" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+
+      <Login>LOGIN</Login>
+    </Nav>
+  );
+};
+
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`;
+
+const Logo = styled.a`
+  padding: 0;
+  cursor: pointer;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  font-size: 0;
+  display: inline-block;
+
+  img {
+    display: block;
+    width: 100%;
+  }
+`;
+
+const NavMenu = styled.div`
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  padding: 0;
+  margin: 0;
+  position: relative;
+  margin-right: auto;
+  margin-left: 25px;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+
+    img {
+      height: 20px;
+      min-width: 20px;
+      width: 20px;
+      z-index: auto;
+    }
+
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.08;
+      padding: 2px 0;
+      white-space: nowrap;
+      position: relative;
+
+      &:before {
+        background-color: rgb(249, 249, 249);
+        border-radius: 0 0 4px 4px;
+        bottom: -6px;
+        content: "";
+        height: 2px;
+        left: 0;
+        opacity: 0;
+        position: absolute;
+        right: 0;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+      }
+    }
+
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const Login = styled.button`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 10px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  color: #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: #090b13;
+    border-color: transparent;
+  }
+`;
+
+export default Header;
