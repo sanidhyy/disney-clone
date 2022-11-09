b09e887a757169af06bfe890cfcfb8c9b525f635
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-11-09 21:12:23 +0530

    assets index generated

---
+++ src/assets/images/index.js
@@ -0,0 +1,56 @@
+// Contains assets including images, logos and other media
+// Don't remove anything from here (if not sure)
+
+// Import assets
+import CTALogo1 from "./cta-logo-one.svg";
+import CTALogo2 from "./cta-logo-two.png";
+import Logo from "./logo.svg";
+import GithubIcon from "./github-icon.svg";
+import PlayBlack from "./play-black.png";
+import PlayWhite from "./play-white.png";
+import GroupIcon from "./group-icon.png";
+import HomeBg from "./home-bg.png";
+import HomeIcon from "./home-icon.svg";
+import SearchIcon from "./search-icon.svg";
+import WatchlistIcon from "./watchlist-icon.svg";
+import OriginalsIcon from "./original-icon.svg";
+import MoviesIcon from "./movie-icon.svg";
+import SeriesIcon from "./series-icon.svg";
+import LoginBg from "./login-bg.jpg";
+import Slider1 from "./slider-badging.jpg";
+import Slider2 from "./slider-scale.jpg";
+import Slider3 from "./slider-badag.jpg";
+import Slider4 from "./slider-scales.jpg";
+import DisneyView from "./viewers-disney.png";
+import MarvelView from "./viewers-marvel.png";
+import NationalView from "./viewers-national.png";
+import PixarView from "./viewers-pixar.png";
+import StarwarsView from "./viewers-starwars.png";
+
+// Export assets
+export {
+  CTALogo1,
+  CTALogo2,
+  Logo,
+  GithubIcon,
+  PlayBlack,
+  PlayWhite,
+  GroupIcon,
+  HomeBg,
+  HomeIcon,
+  SearchIcon,
+  WatchlistIcon,
+  OriginalsIcon,
+  MoviesIcon,
+  SeriesIcon,
+  LoginBg,
+  Slider1,
+  Slider2,
+  Slider3,
+  Slider4,
+  DisneyView,
+  MarvelView,
+  NationalView,
+  PixarView,
+  StarwarsView,
+};
---
+++ src/assets/videos/index.js
@@ -0,0 +1,12 @@
+// Contains videos for web app
+// Don't remove anything from here (if not sure)
+
+// Import assets
+import DisneyVideo from "./video-disney.mp4";
+import MarvelVideo from "./video-marvel.mp4";
+import NationalVideo from "./video-national.mp4";
+import PixarVideo from "./video-pixar.mp4";
+import StarwarsVideo from "./video-starwars.mp4";
+
+// Export assets
+export { DisneyVideo, MarvelVideo, NationalVideo, PixarVideo, StarwarsVideo };
