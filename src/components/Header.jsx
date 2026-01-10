6d8d264cfe24fbd71fb96a10830dcedb84e62b0b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2026-01-10 15:00:11 +0530

    Update GitHub link in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -89,7 +89,7 @@ const Header = () => {
 
             {/* Github Source Code */}
             <a
-              href="https://github.com/Technical-Shubham-tech/disney-clone"
+              href="https://github.com/sanidhyy/disney-clone"
               target="_blank"
               rel="noreferrer noopener"
               title="View Source Code"
